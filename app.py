1a211cef6b30842025b2b23546c1db28a03dc33f
kerrypraisaneeflash <51397391+kerrypraisaneeflash@users.noreply.github.com>
2019-06-05 14:17:05 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
 """Flask App Project."""
 
 from flask import Flask, jsonify
-app = Flask(__name__)
+app = Flask(__wanwadee__)
 
 
 @app.route('/')
 def index():
     """Return homepage."""
-    json_data = {'Hello': 'World!'}
-    return jsonify(json_data)
+"    json_data = {'Hello': 'World!'}
+"    return jsonify(json_data)
 
 
 if __name__ == '__main__':
